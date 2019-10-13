d0a2f6f72b072e4e7a1e1009f16114e4c91ed7f6
Ganesha Danu <ganesha.danu@kumparan.com>
2019-10-13 12:32:30 +0700

    implement design system

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,19 +3,26 @@ function navigateTo(article: string) {
     switch (article) {
         case "home":
             route = home
-            break;
-        case "nothome":
-            route = nothome
-            break;
+            break
         default:
-            break;
+            route = nothome
+            break
     }
 
 
     let converter = new showdown.Converter(),
-        html = converter.makeHtml(route);
+        html = converter.makeHtml(route)
 
-
-    console.log(html)
+    select(article)
     document.getElementById('content').innerHTML = html
+}
+
+function select(article: string) {
+    let items: HTMLCollectionOf<Element> = document.getElementsByClassName("nav-item")
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.remove("selected")
+        if (items[i].id == article) {
+            items[i].classList.add("selected")
+        }
+    }
 }
